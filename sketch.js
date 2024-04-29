02159a5160b330c7e378b502c4177092687e0d4c
fangruiyu <150638449+fangruiyu@users.noreply.github.com>
2024-04-29 11:50:00 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,11 @@ function setup() {
   radioElement.position(width/2-300,20)
   radioElement.option("方塊")
   radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
   radioElement.style("color","#fff")
+  radioElement.style("font-size","30px")
 
 }
 
@@ -31,15 +35,30 @@ function draw() {
     for(var y=0;y<captureGraphics.height;y=y+span){
       var pixel = captureGraphics.get(x,y)
       fill(pixel)
-      if(radioElement.value()=="方塊"){
+      if(radioElement.value()=="方塊"||radioElement.value()=="" ){
         rect(x,y,span)
       }
       if(radioElement.value()=="圓圈"){
         ellipse(x,y,span)
+      }
+      if(radioElement.value()=="亮度"){
+        bk = (pixel[0]+ pixel[1]+ pixel[2])/3
+        fill(bk)
+        //ellipse(x,y,span)//span代表圓圈直徑
+        ellipse(x,y,span*map(bk,0,255,0,1))//透過map指令，bk在0~255間，依照比率設定
+      }
+      if(radioElement.value()=="文字"){
+        const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+        let txt = "一二三四五田雷電龕龘"
+        let bkId = int(map(bk, 0, 255, 9, 0))
+        textSize(span)
+        textStyle(BOLD)
+        text(txt[bkId],x,y)
       }
     }
    }
   pop()
   
 }
+
 
